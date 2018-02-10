85bec3bfb9d7e2a37fc48f05f2089b709e725806
Firexit <workwithpurpose@yahoo.com>
2018-02-10 16:44:35 -0600

    changed java file created global variables for all the views, initialized them in onCreate() method and then used them in my own methods. This will reduce the number of calls to findViewById() method which is resource intensive and expensive work. also extracted all dimensions, colors, strings

--- app/src/main/java/com/example/anna/justjava/MainActivity.java
+++ app/src/main/java/com/example/anna/justjava/MainActivity.java
@@ -7,47 +7,63 @@ import android.widget.TextView;
 
 import java.text.NumberFormat;
 
+import static android.os.Build.ID;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-    int quantity=0;
-    int price1=5;
+    int quantity = 0;
+
+    TextView quantityTextView;
+    TextView priceTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        priceTextView = (TextView) findViewById(R.id.price_text_view);
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void increment(View view) {
-        quantity=quantity+1;
-        display(quantity);
-        displayPrice(price1*quantity);
-    }
-    public void decriment(View view) {
-        quantity=quantity-1;
-        display(quantity);
-        displayPrice(price1*quantity);
+        int price1 = 5;
+        quantity = quantity + 1;
+        price1 = price1 * quantity;
+        quantityTextView.setText(String.valueOf(quantity));
+        priceTextView.setText(String.valueOf(NumberFormat.getCurrencyInstance().format(price1)));
     }
 
+    public void decriment(View view) {
+        int price1 = 5;
+        quantity = quantity - 1;
+        if (quantity <= 0) {
+            quantity = 0;
+        }
+
+        price1 = price1 * quantity;
+        quantityTextView.setText(String.valueOf(quantity));
+        priceTextView.setText(String.valueOf(NumberFormat.getCurrencyInstance().format(price1)));
+    }
+}
 
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+  /**  private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
+   /* private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
-}
+}*/
 
